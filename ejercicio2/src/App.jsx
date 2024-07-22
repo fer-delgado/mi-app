51323d180a08285148dff59adfa7c1f5c6d8c3c3
fer-delgado <fernandodelgado_31@gmail.com>
2024-07-22 10:35:38 -0300

    primer commit

---
+++ ejercicio2/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+export default function App() {
+  
+
+  return (
+    <>
+      <h1>Ejercicio 2</h1>
+    </>
+  )
+}
+
+
---
+++ ejrcicio1/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+export default function App() {
+
+
+  return(
+    <>
+      <h1>ejercicio 1</h1>
+    </>
+  )
+}
